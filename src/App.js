1591af313d3137d87d0d23d891214e9285284774
MrH2310 <mmalinowski2310@gmail.com>
2021-08-01 23:53:58 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.scss';
 
+import  {
+  HashRouter,
+  Route,
+} from "react-router-dom";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Dopiero zaczyna się projekt!!!!</p>
+      <HashRouter>
+        <>
+        <Route exact path ='/' component={Home}/>
+        <Route exact path ='/login' component={Login}/>
+        <Route exact path = '/register' component={Register}/>
+        </>
+        </HashRouter>
       </header>
     </div>
   );
